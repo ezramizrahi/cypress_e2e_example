21ebe998e7c62a6ad2b7101b098c56ed9e5aa22b
ezramizrahi <mizrahi.ez@gmail.com>
2022-11-07 17:12:39 +1100

    change report dir

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     reporter: 'mochawesome',
     reporterOptions: {
-      reportDir: 'public',
+      reportDir: 'mochawesome-report',
       overwrite: true,
       html: false,
       json: true
